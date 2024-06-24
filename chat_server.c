9235f0b621f738f8395c6ac20be730473b1bd318
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2024-06-24 19:26:05 -0300

    first commit

---
+++ chat_server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <openssl/ssl.h>
+#include <opensll/err.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+#define MAX_CLIENTS 100
