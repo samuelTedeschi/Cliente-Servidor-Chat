6775c573b93ae45928bde5f960875403561fe56d
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2024-07-08 13:47:53 -0300

    revisado e reformulado

--- chat_client.c
+++ chat_client.c
@@ -9,3 +9,107 @@
 
 #define PORT 8080
 #define BUF_SIZE 1024
+
+void *receive_messages(void *arg);
+void init_openssl();
+void cleanup_openssl();
+SSL_CTX *create_context();
+void configure_context(SSL_CTX *ctx);
+
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    pthread_t tid;
+    char buffer[BUF_SIZE] = {0};
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    // Inicializa o OpenSSL
+    init_openssl();
+    ctx = create_context();
+    configure_context(ctx);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Cria a estrutura SSL e estabelece uma conexão
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+
+    if (SSL_connect(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+    } else {
+        printf("Conectado ao servidor\n");
+        pthread_create(&tid, NULL, receive_messages, (void *)ssl);
+
+        while (1) {
+            fgets(buffer, BUF_SIZE, stdin);
+            SSL_write(ssl, buffer, strlen(buffer));
+        }
+    }
+
+    close(sock);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
+    return 0;
+}
+
+void *receive_messages(void *arg) {
+    SSL *ssl = (SSL *)arg;
+    char buffer[BUF_SIZE];
+    int nbytes;
+
+    while ((nbytes = SSL_read(ssl, buffer, BUF_SIZE)) > 0) {
+        buffer[nbytes] = '\0';
+        printf("Servidor: %s", buffer);
+    }
+
+    return NULL;
+}
+
+void init_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_client_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    // Configurações adicionais para o contexto SSL
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+}
--- chat_server.c
+++ chat_server.c
@@ -18,22 +18,24 @@ void send_message_to_all(char *message, int exclude_fd);
 void init_openssl();
 void cleanup_openssl();
 SSL_CTX *create_context();
+void configure_context(SSL_CTX *ctx);
 
 int client_sockets[MAX_CLIENTS];
+SSL *client_ssl[MAX_CLIENTS];
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int main (){
-
-    int serve_fd, new_socked;
+int main() {
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    // inicializa o OpenSSL
+    // Inicializa o OpenSSL
     init_openssl();
-    cxt = create_context();
+    ctx = create_context();
+    configure_context(ctx);
 
-    //configurar servidor
-    if ((server_fd = socket(AF_INET, SOCKET_STREAM, 0)) == 0 ){
+    // Configura o servidor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
@@ -42,33 +44,37 @@ int main (){
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (bind(serve_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
-        perror("bind failed");
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0){
-        perror("listen failed");
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Servidor rodando na porta %d\n", PORT);
 
-    while ((new_socked = accept(serve_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0){
+    while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0) {
         pthread_t tid;
         SSL *ssl;
 
-        //criar a estrutura SSL
+        // Cria a estrutura SSL
         ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, new_socked);
+        SSL_set_fd(ssl, new_socket);
 
         if (SSL_accept(ssl) <= 0) {
             ERR_print_errors_fp(stderr);
-        }else {
+            close(new_socket);
+            SSL_free(ssl);
+        } else {
+            printf("Cliente conectado\n");
             pthread_mutex_lock(&clients_mutex);
-            for (int i = 0, i < MAX_CLIENTS; ++i){
-                if (client_sockets[i] == 0){
-                    client_sockets[i] == new_socked;
+            for (int i = 0; i < MAX_CLIENTS; ++i) {
+                if (client_sockets[i] == 0) {
+                    client_sockets[i] = new_socket;
+                    client_ssl[i] = ssl;
                     pthread_create(&tid, NULL, handle_client, (void *)ssl);
                     break;
                 }
@@ -77,19 +83,19 @@ int main (){
         }
     }
 
-    close(serve_fd);
+    close(server_fd);
     cleanup_openssl();
     return 0;
-
 }
 
-void *handle_client(void *arg){
-    SSL *sll = (SSL *)arg;
+void *handle_client(void *arg) {
+    SSL *ssl = (SSL *)arg;
     char buffer[BUF_SIZE];
     int nbytes;
 
-    while((nbytes = SSL_read(ssl, buffer, BUF_SIZE)) > 0){
+    while ((nbytes = SSL_read(ssl, buffer, BUF_SIZE)) > 0) {
         buffer[nbytes] = '\0';
+        printf("Mensagem recebida: %s", buffer);
         send_message_to_all(buffer, SSL_get_fd(ssl));
     }
 
@@ -97,9 +103,10 @@ void *handle_client(void *arg){
     SSL_free(ssl);
 
     pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i){
-        if (client_sockets[i] == SSl_get_fd(ssl)) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (client_sockets[i] == SSL_get_fd(ssl)) {
             client_sockets[i] = 0;
+            client_ssl[i] = NULL;
             break;
         }
     }
@@ -108,37 +115,53 @@ void *handle_client(void *arg){
     return NULL;
 }
 
-void send_message_to_all(char *message, int exclude_fd){
+void send_message_to_all(char *message, int exclude_fd) {
     pthread_mutex_lock(&clients_mutex);
-    for ( int i = 0; i < MAX_CLIENTS; ++i){
-        if (client_sockets[i] != 0 && client_sockets[i] != exclude_fd){
-            SSL_write(SSL_new(ctx), message, strlen(message));
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (client_sockets[i] != 0 && client_sockets[i] != exclude_fd) {
+            SSL_write(client_ssl[i], message, strlen(message));
         }
     }
     pthread_mutex_unlock(&clients_mutex);
 }
 
-void init_openssl(){
+void init_openssl() {
     SSL_load_error_strings();
-    OpenSLL_add_ssl_algorithms();
+    OpenSSL_add_ssl_algorithms();
 }
 
-void cleanup_openssl(){
+void cleanup_openssl() {
     EVP_cleanup();
 }
 
-SLL_CTX *create_context(){
+SSL_CTX *create_context() {
     const SSL_METHOD *method;
     SSL_CTX *ctx;
 
     method = SSLv23_server_method();
 
     ctx = SSL_CTX_new(method);
-    if (!ctx){
-        perror("unable to create ssl context");
+    if (!ctx) {
+        perror("Unable to create SSL context");
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
     return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    // Configurações adicionais para o contexto SSL
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    // Carrega os certificados do servidor
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
 }
